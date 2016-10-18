1608292e447ad960b0c6c57ec69db7ee5d2f99ab
okrand <orkun.krand@gmail.com>
2016-10-18 01:15:37 -0400

    Focusing on username on login page load

--- login.php
+++ login.php
@@ -12,6 +12,9 @@ session_start();
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        document.getElementById("username").focus();
+    </script>
 </head>
 <body>
         <header class="jumbotron text-center" style="background-color:white;">
