24e354042d00f7420d558ce72aa2c98707c7f8ff
okrand <orkun.krand@gmail.com>
2016-10-18 00:35:18 -0400

    Minor Changes

--- correctans.php
+++ correctans.php
@@ -8,7 +8,7 @@ $correctanswerid = $_POST["AnswerSubmit"];
 $correctquery = 'UPDATE QUESTIONS SET ANSWER_ID=' . $correctanswerid . ' WHERE ID='. $_SESSION["QNumber"];
 $corr = sqlcommand($correctquery, "UPDATE");
 //echo '<meta http-equiv="refresh" content="2;url=question.php"/>';
-header('location: question.php/');
+header('location: question.php');
 exit();
 session_write_close();
 ?>
