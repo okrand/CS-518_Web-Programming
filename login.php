cffcfe239d359e80d61e378bfabb6537616ee012
okrand <orkun.krand@gmail.com>
2016-10-30 21:43:14 -0400

    login issues

--- login.php
+++ login.php
@@ -68,15 +68,14 @@ session_start();
         if (isset($_SERVER["HTTP_REFERER"]))
             $referer = pagename($_SERVER["HTTP_REFERER"]);
         else
-            $referer = "/login.php";
-        if ($referer != "/login.php" && $referer != "/profile.php")
+            $referer = "this";
+        if ($referer != "this" && $referer != "/profile.php")
             $_SESSION["referer"] = $referer;
         else
             $_SESSION["referer"] = "/index.php";
         
-        echo $_SESSION["referer"];
+    echo $_SESSION["referer"];
     }
-    
 ?>
     <div class="container">
 	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
