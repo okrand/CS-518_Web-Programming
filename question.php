be5804fefcde6da50d59a29b6ceefda3c8d8b652
okrand <orkun.krand@gmail.com>
2016-11-08 16:29:02 -0500

    Trying to fix redirect

--- question.php
+++ question.php
@@ -152,7 +152,7 @@ else document.getElementById("newAnswer").submit();
             $answer = test_input($_POST["Answer"]);
             $query = "INSERT INTO ANSWERS (QUEST_ID, USER_ID, ANSWER,DATE_ANSWERED) VALUES (".$_SESSION["QNumber"].", ".$_SESSION["UserID"].", '".$answer."', NOW());";
             $sqlresult = sqlcommand($query, "INSERT");
-            $url = "/question.php";
+            $url = '/question.php';
             redirect($url);
     }
     ?>
