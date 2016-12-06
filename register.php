9d3663017d9b46a75e5430c81c5041e401425d3d
okrand <orkun.krand@gmail.com>
2016-12-06 13:27:53 -0500

    annoying captcha

--- register.php
+++ register.php
@@ -37,18 +37,17 @@ session_start();
         elseif (strlen($pass) == 0)
             echo "<div align='center' class='alert alert-warning'><strong>You can't have a blank password dummy</strong></div>";
             
-        if(!isset($_POST['g-recaptcha-response'])){
+        if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+        }
+        if(!$captcha){
             echo "<div align='center' class='alert alert-warning'><strong>You forgot the captcha buddy</strong></div>";
         }
-        //if(!$captcha){
-        else{
-            $captcha=$_POST['g-recaptcha-response'];
-        
         $secretKey = "6Lfk8A0UAAAAAKWJR_aOwmu3BUcNOZBTWJAnvg--";
         $ip = $_SERVER['REMOTE_ADDR'];
         $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
         $responseKeys = json_decode($response,true);
-        if(intval($responseKeys["success"]) !== 1) {
+        if($captcha && (intval($responseKeys["success"]) !== 1)) {
           echo "<div align='center' class='alert alert-warning'><strong>You are a spammer, GET OUT!</strong></div>";
           exit;
         } 
@@ -72,7 +71,6 @@ session_start();
             redirect($_SESSION["referer"]);
         }
     }
-        }
 	}
     else{
         if (isset($_SERVER["HTTP_REFERER"]))
